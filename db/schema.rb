5f8f662077306d93eb5d926bb4cbd045c76da140
Alex Ravan <alexravan@gmail.com>
2016-02-24 11:30:48 -0500

    removed video

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160224152159) do
+ActiveRecord::Schema.define(version: 20160224162141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,12 +42,6 @@ ActiveRecord::Schema.define(version: 20160224152159) do
     t.datetime "updated_at",      null: false
     t.integer  "image_table"
     t.integer  "video_table"
-  end
-
-  create_table "videos", force: :cascade do |t|
-    t.string   "link"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
