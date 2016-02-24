4709ef64b3f02485bb96e08c4e280d2c90e52026
Jacob Katsiaficas <jacob.katsiaficas@tufts.edu>
2016-02-24 10:15:32 -0500

    got tests working with rspec & factory girl

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -1,6 +1,6 @@
 class Incident < ActiveRecord::Base
-	# validates :user_id, presence: true
-	# validates :category, presence: true
+	validates :user_id, presence: true
+	validates :category, presence: true
 	validates :location, presence: true
 	validates :title, presence: true
 	validates :severity, presence: true
