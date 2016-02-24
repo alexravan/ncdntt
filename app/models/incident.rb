dc0653b49d1e7ab5af32e4888f3b369c87f0f339
Mateo Davis <mateo.cohen.davis@gmail.com>
2016-02-24 15:39:07 -0500

    added paperclip

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -4,4 +4,12 @@ class Incident < ActiveRecord::Base
 	validates :location, presence: true
 	validates :title, presence: true
 	validates :severity, presence: true
+	 # This method associates the attribute ":avatar" with a file attachment
+	has_attached_file :media, styles: {
+		thumb: '100x100>',
+		square: '200x200#',
+		medium: '300x300>'
+	}
+	# Validate the attached image is image/jpg, image/png, etc
+	validates_attachment_content_type :media, :content_type => /\Aimage\/.*\Z/
 end
