69586bd647870bd15dcca24341ca550893f8bf1b
Mateo Davis <mateo.cohen.davis@gmail.com>
2016-02-28 12:22:32 -0500

    testing images

--- ncdntt/db/schema.rb
+++ ncdntt/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160228162830) do
+ActiveRecord::Schema.define(version: 20160228171833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,12 +39,8 @@ ActiveRecord::Schema.define(version: 20160228162830) do
     t.boolean  "is_closed"
     t.datetime "date_closed"
     t.string   "closing_comment"
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
-    t.string   "picture_file_name"
-    t.string   "picture_content_type"
-    t.integer  "picture_file_size"
-    t.datetime "picture_updated_at"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.string   "media_file_name"
     t.string   "media_content_type"
     t.integer  "media_file_size"
