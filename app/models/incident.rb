ad267bef95b2b26a0fa6be3659dddf9d4f197b44
Mateo Davis <mateo.cohen.davis@gmail.com>
2016-03-09 11:49:23 -0500

    began on devise

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -1,5 +1,4 @@
 class Incident < ActiveRecord::Base
-	validates :user_id, presence: true
 	validates :category, presence: true
 	validates :location, presence: true
 	validates :title, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160305212330) do
+ActiveRecord::Schema.define(version: 20160309163424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,16 +22,8 @@ ActiveRecord::Schema.define(version: 20160305212330) do
     t.string   "name"
   end
 
-  create_table "images", force: :cascade do |t|
-    t.string   "link"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.integer  "incident_id"
-  end
-
   create_table "incidents", force: :cascade do |t|
     t.string   "title"
-    t.integer  "user_id"
     t.integer  "category"
     t.string   "description"
     t.integer  "severity"
@@ -46,5 +38,23 @@ ActiveRecord::Schema.define(version: 20160305212330) do
     t.integer  "media_file_size"
     t.datetime "media_updated_at"
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
