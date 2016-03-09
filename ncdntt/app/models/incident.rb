6c6495b97cca06c8507549e91f46dfb8eca24a8d
Mateo Davis <mateo.cohen.davis@gmail.com>
2016-03-09 16:17:07 -0500

    finished categories

--- ncdntt/app/models/incident.rb
+++ ncdntt/app/models/incident.rb
@@ -1,11 +1,11 @@
 class Incident < ActiveRecord::Base
 	validates :user_id, presence: true
-	validates :category, presence: true
+	# validates :category_id, presence: true
 	validates :location, presence: true
 	validates :title, presence: true
 	validates :severity, presence: true
 	 # This method associates the attribute ":avatar" with a file attachment
-	 # belongs_to :category
+	belongs_to :category
 	has_attached_file :media, styles: {
 		thumb: '100x100>',
 		square: '200x200#',
--- ncdntt/db/schema.rb
+++ ncdntt/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160305212330) do
+ActiveRecord::Schema.define(version: 20160309195937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 20160305212330) do
   create_table "incidents", force: :cascade do |t|
     t.string   "title"
     t.integer  "user_id"
-    t.integer  "category"
     t.string   "description"
     t.integer  "severity"
     t.string   "location"
@@ -45,6 +44,7 @@ ActiveRecord::Schema.define(version: 20160305212330) do
     t.string   "media_content_type"
     t.integer  "media_file_size"
     t.datetime "media_updated_at"
+    t.integer  "category_id"
   end
 
 end
