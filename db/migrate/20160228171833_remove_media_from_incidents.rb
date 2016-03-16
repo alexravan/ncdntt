b9a32fe051032179158f655fe09cff501dc0a64c
Mateo Davis <mateo.cohen.davis@gmail.com>
2016-03-16 11:14:09 -0400

    moved video drop migration for heroku

--- db/migrate/20160228171833_remove_media_from_incidents.rb
+++
@@ -1,8 +0,0 @@
-class RemoveMediaFromIncidents < ActiveRecord::Migration
-  def change
-  	remove_column :incidents, :picture_file_name, :string
-  	remove_column :incidents, :picture_content_type, :string
-  	remove_column :incidents, :picture_file_size, :integer
-  	remove_column :incidents, :picture_updated_at, :datetime
-  end
-end
