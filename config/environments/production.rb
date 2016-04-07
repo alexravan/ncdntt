7d7fa05bc2d94215ea6026b1565325cbf0f6e27d
Mateo Davis <mateo.cohen.davis@gmail.com>
2016-04-07 14:34:19 -0400

    fixed env variables in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,4 +87,12 @@ config.static_cache_control = "public, max-age=31536000"
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
 }
+config.cache_store = :dalli_store,
+                    (ENV["MEMCACHIER_SERVERS"] || "").split(","),
+                    {:username => ENV["MEMCACHIER_USERNAME"],
+                     :password => ENV["MEMCACHIER_PASSWORD"],
+                     :failover => true,
+                     :socket_timeout => 1.5,
+                     :socket_failure_delay => 0.2
+                    }
 end
