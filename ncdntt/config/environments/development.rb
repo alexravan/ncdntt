9491164b6f0902c72c4f054dd7b23c4963777afe
Mateo Davis <mateo.cohen.davis@gmail.com>
2016-04-08 20:39:39 -0400

    test2

--- ncdntt/config/environments/development.rb
+++ ncdntt/config/environments/development.rb
@@ -42,4 +42,14 @@ Rails.application.configure do
     :storage => :s3,
     :bucket => 'ncdntt'
   }
+
+  config.cache_store = :dalli_store,
+                    (ENV["MEMCACHIER_SERVERS"] || "").split(","),
+                    {:username => ENV["MEMCACHIER_USERNAME"],
+                     :password => ENV["MEMCACHIER_PASSWORD"],
+                     :failover => true,
+                     :socket_timeout => 1.5,
+                     :socket_failure_delay => 0.2
+                    }
+
 end
